32cf56699070901d7add6e0dd3633b2129aacd08
wainwrightmark <wainwrightmark@users.noreply.github.com>
2022-06-24 19:44:33 +0000

    deploy: 8fd71bf3a486ee62be1528fb5d1e583edf55d7e3

--- convext-3b7afefacad02251.js
+++ convext-3b7afefacad02251.js
@@ -284,7 +284,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('convext-3ceab751bf31ef79_bg.wasm', import.meta.url);
+        input = new URL('convext-3b7afefacad02251_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
